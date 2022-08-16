d937d9c41dcc11c4cfacd88d34310aea065a0cf4
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2022-08-17 07:47:31 +0900

    :card_file_box: Finish migrating for notification

---
+++ src/infrastructure/rdb/entities/notification-entity.ts
@@ -0,0 +1,49 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import BaseEntity from './base';
+import User from './user-entity';
+
+const NotificationType = {
+  FollowNotify: 'FollowNotify',
+  PostLikeNotify: 'PostLikeNotify',
+  PostCommentNotify: 'PostCommentNotify',
+} as const;
+
+export type NotificationType =
+  typeof NotificationType[keyof typeof NotificationType];
+
+@Entity()
+export default class Notification extends BaseEntity {
+  @Column()
+  ownerId: number;
+
+  @Column()
+  sourceUserId: number;
+
+  @Column()
+  content: string;
+
+  @Column({
+    nullable: true,
+  })
+  postId: number;
+
+  @Column({
+    type: 'enum',
+    enum: NotificationType,
+  })
+  type: NotificationType;
+
+  @Column()
+  read: boolean;
+
+  @Column()
+  image: string;
+
+  @ManyToOne((type) => User, (user) => user.ownNotifications)
+  @JoinColumn({ name: 'owner_id' })
+  public owner: User;
+
+  @ManyToOne((type) => User, (user) => user.sentNotifications)
+  @JoinColumn({ name: 'source_user_id' })
+  public sourceUser: User;
+}
