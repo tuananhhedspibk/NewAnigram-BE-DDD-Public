cde1f4175538098c332cacf0d45f68f9026aebab
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2022-08-16 23:42:30 +0900

    :card_file_box: Finish migrating for post

--- src/infrastructure/rdb/entities/user-entity.ts
+++ src/infrastructure/rdb/entities/user-entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToOne } from 'typeorm';
+import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import BaseEntity from './base';
+import Post from './post';
 import UserDetail from './uset-detail-entity';
 
 @Entity()
@@ -17,4 +18,7 @@ export default class User extends BaseEntity {
 
   @OneToOne((type) => UserDetail, (userDetail) => userDetail.user)
   public userDetail: UserDetail;
+
+  @OneToMany((type) => Post, (post) => post.user)
+  public posts: Post[];
 }
