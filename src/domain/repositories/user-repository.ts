9a20325046d34fd6b7a6cc81d318c5c391d7ed11
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2022-08-27 17:59:36 +0900

    :tada: Complete user domain repositories

---
+++ src/domain/repositories/user-repository.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from '../entities/user';
+import { BaseRepository } from './base';
+
+export abstract class IUserRepository extends BaseRepository {
+  getByEmail: (
+    transaction: TransactionType | null,
+    email: string,
+  ) => Promise<UserEntity | null>;
+}
