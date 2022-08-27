ce48bd9b3c701497faec10833ed31e81c25371dd
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2022-08-27 13:54:10 +0900

    :rotating_light: Fix lint

--- src/infrastructure/rdb/entities/user-entity.ts
+++ src/infrastructure/rdb/entities/user-entity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import BaseEntity from './base';
 import Post from './post-entity';
@@ -23,27 +24,28 @@ export default class User extends BaseEntity {
   @Column()
   salt: string;
 
-  @OneToOne((type) => UserDetail, (userDetail) => userDetail.user)
+  @OneToOne((_type) => UserDetail, (userDetail) => userDetail.user)
   public userDetail: UserDetail;
 
-  @OneToMany((type) => Post, (post) => post.user)
+  @OneToMany((_type) => Post, (post) => post.user)
   public posts: Post[];
 
-  @OneToMany((type) => Like, (like) => like.user)
+  @OneToMany((_type) => Like, (like) => like.user)
   public likes: Like[];
 
-  @OneToMany((type) => Comment, (comment) => comment.user)
+  @OneToMany((_type) => Comment, (comment) => comment.user)
   public comments: Comment[];
 
-  @OneToMany((type) => Notification, (notification) => notification.sourceUser)
+  @OneToMany((_type) => Notification, (notification) => notification.sourceUser)
   public sentNotifications: Notification[];
 
-  @OneToMany((type) => Notification, (notification) => notification.owner)
+  @OneToMany((_type) => Notification, (notification) => notification.owner)
   public ownNotifications: Notification[];
 
-  @OneToMany((type) => Follow, (follow) => follow.sourceUser)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @OneToMany((_type) => Follow, (follow) => follow.sourceUser)
   public followers: Follow[];
 
-  @OneToMany((type) => Follow, (follow) => follow.destinationUser)
+  @OneToMany((_type) => Follow, (follow) => follow.destinationUser)
   public followings: Follow[];
 }
